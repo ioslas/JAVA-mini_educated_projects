4b268d97d5735fa99b532b199b5b3bbcf78cc603
ioslas <iosifvlas@gmail.com>
2025-05-22 13:03:26 +0300

    Add files via upload

---
+++ Medium_intermediate_level/Desktop_Development/Cryptography/src/RSA.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.security.*;
+import java.util.Base64;
+import javax.crypto.*;
+
+public class RSA extends AsymmetricEncDec{
+	private static final int RSA_key_size = 2048;
+	private static final int CHUNK_size = (RSA_key_size/8) - 11;// Max chunk size for encryption with padding
+	public KeyPair generateKeyPair() throws Exception {
+		KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+		keyGen.initialize(RSA_key_size);
+		return keyGen.generateKeyPair();
+	}
+	@Override
+	public void encryptFile(String input, String output, PublicKey pk) throws Exception {
+		// TODO Auto-generated method stub
+		// Read file as bytes
+		// byte[] data = super.readFile(input);
+		try(FileInputStream fis = new FileInputStream(input);
+			FileOutputStream fos = new FileOutputStream(output)){
+			
+			byte[] buffer = new byte[CHUNK_size];
+			int bytesRead;
+			while ((bytesRead = fis.read(buffer)) != -1) {
+				// Trim a byte data to buffer CHUNK_size
+				byte[] data = bytesRead == buffer.length ? buffer: trimByteArray(buffer, bytesRead);
+				//Encrypt Data as chunk
+				Cipher cipher = Cipher.getInstance("RSA");
+				cipher.init(Cipher.ENCRYPT_MODE, pk);
+				byte[] encryptedData = cipher.doFinal(data);
+				// Write the encrypted data(chunk) with Base64 encoding for portability
+				fos.write(Base64.getEncoder().encode(encryptedData));
+				fos.write('\n'); // Add a newline for data (chunk) separation
+			}
+		}
+		// Write encrypted file 
+		//super.writeFile(output, encryptedData);
+	}
+	// Utility method to trim a byte array to the specified length
+	private static byte[] trimByteArray(byte[] array, int length) {
+		// TODO Auto-generated method stub
+		byte[] trimmed = new byte[length];
+		System.arraycopy(array, 0, trimmed, 0, length);
+		return trimmed;
+	}
+	@Override
+	public void decryptFile(String encrypted, String output, PrivateKey pk) throws Exception {
+		// TODO Auto-generated method stub
+		// Read file as bytes
+		// byte[] encryptedData = super.readFile(input);
+		try(BufferedReader br = new BufferedReader(new FileReader(encrypted));
+			FileOutputStream fos = new FileOutputStream(output)) {
+			
+			String line;
+			while((line = br.readLine()) != null) {
+				// Decrypt the encrypted data (chunk)
+				byte[] encryptedChunk = Base64.getDecoder().decode(line);
+				Cipher cipher = Cipher.getInstance("RSA");
+				cipher.init(Cipher.DECRYPT_MODE, pk);
+				byte[] decryptedData = cipher.doFinal(encryptedChunk);
+                // Write the decrypted data to the output file
+				fos.write(decryptedData);
+			}
+		}
+		// Write decrypted file
+		//super.writeFile(output, decryptedData);
+	}
+}
